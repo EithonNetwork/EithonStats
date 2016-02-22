f8f500320fc135e787b7933b93ec42bb40c81651
vanbrayne <lars.lindberg@xlent.se>
2016-02-22 20:11:57 +0100

    Bugfixing

--- src/main/java/net/eithon/plugin/stats/logic/HourStatistics.java
+++ src/main/java/net/eithon/plugin/stats/logic/HourStatistics.java
@@ -1,7 +1,6 @@
 package net.eithon.plugin.stats.logic;
 
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
@@ -15,64 +14,85 @@ import net.eithon.plugin.stats.Config;
 import net.eithon.plugin.stats.db.TimeSpan;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 
 public class HourStatistics implements IUuidAndName {
 
 	// Saved variables
 	private UUID _playerId;
-	private String _playerName;
 	private long _blocksBroken;
 	private long _blocksCreated;
 	private long _chatActivities;
 	private long _playtimeInSeconds;
 	private LocalDateTime _hour;
-	private long _dbId;
 
-	public HourStatistics(Database database, HourStatistics earlier, PlayerStatistics now, LocalDateTime time) throws SQLException, ClassNotFoundException {
-		HourStatistics later = new HourStatistics(now, time);
-		this._playerId = earlier._playerId;
-		this._playerName = earlier._playerName;
-		this._hour = later._hour;
-		this._blocksBroken = later._blocksBroken - earlier._blocksBroken;
-		this._blocksCreated = later._blocksCreated - earlier._blocksCreated;
-		this._chatActivities = later._chatActivities - earlier._chatActivities;
-		this._playtimeInSeconds = later._playtimeInSeconds - earlier._playtimeInSeconds;
-		insertTimeSpan(database, this._hour);
-		toDb(database);
+	// Non-saved variables
+	private Database _database;
+
+	public static HourStatistics save(Database database, HourStatistics earlier, PlayerStatistics now) throws SQLException, ClassNotFoundException {
+		LocalDateTime laterHour = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
+		UUID playerId = earlier._playerId;
+		LocalDateTime earlierHour = earlier._hour;
+		long blocksBroken = now.getBlocksBroken()- earlier._blocksCreated;
+		long blocksCreated = now.getBlocksCreated() - earlier._blocksCreated;
+		long chatActivities = now.getChatMessages() - earlier._chatActivities;
+		long playtimeInSeconds = now.getTotalTimeInSeconds() - earlier._playtimeInSeconds;
+		TimeSpan timeSpan = TimeSpan.getByPlayerIdHour(database, earlier._playerId, earlierHour);
+		if (timeSpan == null) {
+			timeSpan = TimeSpan.create(database, playerId, earlierHour, playtimeInSeconds, 
+					chatActivities, blocksCreated, blocksBroken);
+		} else {
+			timeSpan.update(playtimeInSeconds, chatActivities, blocksCreated, blocksBroken);
+		}
+		if (earlierHour.equals(laterHour)) return earlier;
+		return new HourStatistics(database, now, laterHour);
 	}
 
 	public HourStatistics(Database database, EithonPlayer player, LocalDateTime fromTime, LocalDateTime toTime) throws SQLException, ClassNotFoundException
 	{
+		this._database = database;
 		TimeSpan timeSpan = TimeSpan.sumPlayer(database, player.getUniqueId(), fromTime, toTime);
-		this._playerId = player.getUniqueId();
-		this._playerName = player.getName();
-		this._hour = fromTime.truncatedTo(ChronoUnit.HOURS);
 		fromDb(timeSpan);
+		this._playerId = player.getUniqueId();
+		this._hour = fromTime.truncatedTo(ChronoUnit.HOURS);
 		return;
 	}
 
-	HourStatistics(PlayerStatistics playerStatistics, LocalDateTime time)
+	HourStatistics(Database database, PlayerStatistics playerStatistics, LocalDateTime time)
 	{
+		this._database = database;
+		useCurrentPlayerStatisticsAsBaseline(playerStatistics, time);
+		try {
+			substractAnyExistingHourValues();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	private void useCurrentPlayerStatisticsAsBaseline(
+			PlayerStatistics playerStatistics, LocalDateTime time) {
 		this._playerId = playerStatistics.getUniqueId();
-		this._playerName = playerStatistics.getName();
 		this._hour = time.truncatedTo(ChronoUnit.HOURS);
 		this._blocksBroken = playerStatistics.getBlocksBroken();
 		this._blocksCreated = playerStatistics.getBlocksCreated();
 		this._chatActivities = playerStatistics.getChatMessages();
 		this._playtimeInSeconds = playerStatistics.getTotalTimeInSeconds();
-		this._dbId = -1;
+	}
+
+	private void substractAnyExistingHourValues() throws ClassNotFoundException, SQLException {
+		TimeSpan timeSpan = TimeSpan.getByPlayerIdHour(this._database, this._playerId, this._hour);
+		if (timeSpan == null) return;
+		this._blocksBroken -= timeSpan.get_blocksBroken();
+		this._blocksCreated -= timeSpan.get_blocksCreated();
+		this._chatActivities -= timeSpan.get_chatMessages();
+		this._playtimeInSeconds -= timeSpan.get_playTimeInSeconds();
 	}
 
 	public HourStatistics() {
 		initialize();
-	}
-
-	private void insertTimeSpan(Database database, LocalDateTime hour)
-			throws SQLException, ClassNotFoundException {
-		TimeSpan timeSpan = TimeSpan.create(database, this._playerId, hour, this._playtimeInSeconds, 
-				this._chatActivities, this._blocksCreated, this._blocksBroken);
-		this._dbId = timeSpan.get_dbId();
 	}
 
 	private void initialize() {
@@ -84,8 +104,6 @@ public class HourStatistics implements IUuidAndName {
 
 	private HourStatistics fromDb(TimeSpan timeSpan)  {
 		this._playerId = timeSpan.get_playerId();
-		this._playerName = Bukkit.getServer().getOfflinePlayer(this._playerId).getName();
-		this._dbId = timeSpan.get_dbId();
 		this._chatActivities = timeSpan.get_chatMessages();
 		this._blocksCreated = timeSpan.get_blocksCreated();
 		this._blocksBroken = timeSpan.get_blocksBroken();
@@ -93,25 +111,10 @@ public class HourStatistics implements IUuidAndName {
 		return this;
 	}
 
-	public void toDb(Database database) throws SQLException, ClassNotFoundException {
-		String updates = getDbUpdates();
-		String update = String.format("UPDATE accumulated SET %s WHERE id=%d", updates, this._dbId);
-		Statement statement = database.getOrOpenConnection().createStatement();
-		statement.executeUpdate(update);
-	}
-
-	private String getDbUpdates() {
-		String updates = String.format("player_id='%s'", this._playerId) +
-				String.format(", player_name='%s'", this._playerName) +
-				String.format(", chat_messages=%d", this._chatActivities) +
-				String.format(", blocks_created=%d", this._blocksCreated) +
-				String.format(", blocks_broken=%d", this._blocksBroken) +
-				String.format(", playtime_in_seconds=%d", this._playtimeInSeconds);
-		return updates;
-	}
-
 	public String getName() {
-		return this._playerName;
+		OfflinePlayer player = Bukkit.getServer().getOfflinePlayer(this._playerId);
+		if (player == null) return "Unknown";
+		return player.getName();
 	}
 
 	public UUID getUniqueId() { 
@@ -124,7 +127,7 @@ public class HourStatistics implements IUuidAndName {
 
 	private HashMap<String,String> getNamedArguments() {
 		HashMap<String,String> namedArguments = new HashMap<String, String>();
-		namedArguments.put("PLAYER_NAME", this._playerName);
+		namedArguments.put("PLAYER_NAME", this.getName());
 		namedArguments.put("BLOCKS_BROKEN", String.format("%d", this._blocksBroken));
 		namedArguments.put("BLOCKS_CREATED", String.format("%d", this._blocksCreated));
 		namedArguments.put("BLOCKS_CREATED_OR_BROKEN", String.format("%d", this._blocksCreated + this._blocksBroken));
